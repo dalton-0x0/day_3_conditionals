861abaf4e169ca2645fcfa8e478041278468315f
Dalton Okechukwu <dalton.0x0@gmail.com>
2023-12-27 23:45:37 -0700

    initial commit day_3

---
+++ main.py
@@ -0,0 +1,9 @@
+# day 3 conditionals
+
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height in cm? "))
+
+if height >= 120:
+    print("You can ride the rollercoaster!")
+else:
+    print("Sorry, you have to be taller to ride : )")
